2f8226bcb9099d26270becd2a79c436d10e0727b
2anGel1 <angeemmanuelassamoi@gmail.com>
2023-01-28 00:09:23 +0000

    up

--- resources/views/pdf/commandes.blade.php
+++ resources/views/pdf/commandes.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Liste des Equipes Inscrites</title>
 
@@ -9,7 +10,7 @@
             margin: 25px 0;
             font-size: 0.9em;
             font-family: sans-serif;
-            
+
             box-shadow: 0 0 20px rgba(59, 57, 57, 0.15);
         }
 
@@ -22,8 +23,8 @@
         .styled-table th,
         .styled-table td {
             padding: 12px 15px;
-            text-align:left;
-            
+            text-align: left;
+
         }
 
         .styled-table tbody tr {
@@ -47,50 +48,53 @@
         les cadres oh
         */
 
-        .business-3{
+        .business-3 {
             padding: 30px 0px;
-            display: table;   
+            display: table;
         }
 
-        .card{
-            
-            width: 31%; 
-            
+        .card {
+
+            width: 31%;
+
             display: inline-block;
             border-radius: 10px;
-            bo
-            margin: 0px 1%;
+            bo margin: 0px 1%;
         }
 
-        .card-div{
+        .card-div {
             padding: 10px;
         }
-        .card-div-2{
+
+        .card-div-2 {
             text-align: center;
         }
-        .card-div-1{
+
+        .card-div-1 {
             text-align: right;
         }
 
 
         /**/
 
-        table, td{
+        table,
+        td {
             border: 1px solid black;
         }
-        td{
+
+        td {
             padding: 10px 20px;
         }
-        
-        .table{
+
+        .table {
             width: 100%;
-            border-collapse:collapse;
+            border-collapse: collapse;
 
         }
-        
     </style>
 
 </head>
+
 <body>
 
     <div style="
@@ -98,78 +102,77 @@
  
             width:100%;
             ">
-        
-        <div style=" width:48%; display:inline-block;" >
-           
+
+        <div style=" width:48%; display:inline-block;">
+
             <img alt="Logo de l'entreprise" src="{{asset('images/app/logoC2E-PhotoRoom.png')}}" style="height: 80px; width:130px" />
-        
+
         </div>
 
-        <div style=" width:48%;text-align:right; display:inline-block;" >
-           
+        <div style=" width:48%;text-align:right; display:inline-block;">
+
             <img alt="Logo de l'entreprise" src="{{asset('images/app/logoHackathon-PhotoRoom.png')}}" style="height: 100px; width:130px" />
-        
+
         </div>
 
 
     </div>
 
     <div style="margin:30px 0px; text-align:center; width:100% ;">
-       <span style="font-size:20px; font-weight:bold"> Repartition des Commandes par salle  </span> 
+        <span style="font-size:20px; font-weight:bold"> Repartition des Commandes par salle </span>
     </div>
 
     <table class="table">
         <thead>
             <tr>
                 <th>Nom de la salle</th>
-                
-                <th >commandes</th>
+
+                <th>commandes</th>
             </tr>
         </thead>
-       
+
         @foreach ($salles as $salle)
-            <tr>
-                <td>
-                    {{$salle->libelle }}
-                </td>
-                <td>
-                    <!-- @foreach ($salle->currentEquipes() as  $equipe) -->
-                        @foreach ($collations as $collation)
-                            
-                            <p style="margin: 0; text-transform: capitalize">
-                                <span style="font-weight: bold;">
-                                    {{$collation->libelle}} :
-                                </span>
-                                {{$salle->getNbCollation($collation->id)}} 
-                            </p>
-                            
-                        @endforeach
-                    <!-- @endforeach -->
-                </td>
-            </tr>
+        <tr>
+            <td>
+                {{$salle->libelle }}
+            </td>
+            <td>
+                @foreach ($collations as $collation)
+
+                <p style="margin: 0; text-transform: capitalize">
+                    <span style="font-weight: bold;">
+                        {{$collation->libelle}} :
+                    </span>
+                    {{$salle->getNbCollation($collation->id)}}
+                </p>
+
+                @endforeach
+            </td>
+        </tr>
         @endforeach
 
-   </table>
+    </table>
 
-  
-   <div style="margin: 20px 10px; text-align:center;">
+
+    <div style="margin: 20px 10px; text-align:center;">
 
         <p style="color: red">
             Résumé
         </p>
 
         @foreach ($collations as $collation)
-                                
-            <p style="margin: 0; font-size:16px; text-transform: capitalize">
-                <span style="font-weight: bold;">
-                    {{$collation->libelle}} :
-                </span>
-                {{$collation->commandes()->count()}} commande(s)
-            </p>
-            
+
+        <p style="margin: 0; font-size:16px; text-transform: capitalize">
+            <span style="font-weight: bold;">
+                {{$collation->libelle}} :
+            </span>
+            {{$collation->commandes()->count()}} commande(s)
+        </p>
+
         @endforeach
 
     </div>
-        
+
 </body>
+
 </html>
