6462c9625813a358ae80b4b3f785ec7c9ecd810b
scopy <153370833+angeassamoi@users.noreply.github.com>
2024-02-15 23:35:02 +0000

    quiz-controller-v6

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -18,7 +18,7 @@ class QuizController extends Controller
     */
     public function renderquiz(Request $request)
     {
-        $quiz = Quiz::with('questions.responses')->where('id', Auth::user()->etudiant->getEquipe()->qsession->quiz->id);
+        $quiz = Quiz::with('questions.responses')->where('id', $request->user()->etudiant->getEquipe()->qsession->quiz_id);
 
         // $data = [];
         // foreach ($quiz->questions as $question) {
@@ -44,7 +44,7 @@ class QuizController extends Controller
 
         $response = [
             'status' => true,
-            'questions' => $request->user()->etudiant->getEquipe()->qsession,
+            'questions' => $quiz,
         ];
 
         return response()->json($response);
